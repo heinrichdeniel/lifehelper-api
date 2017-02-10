08fa4f8f0969d63feadd7a65720ec929e9ed9651
Heinrich Deniel <denielheinrich@gmail.com>
2017-02-10 20:17:56 +0200

    Create task extended with update task

--- controller/tasks.js
+++ controller/tasks.js
@@ -3,24 +3,47 @@ var User   = require('../database').User;
 
 //creating a new task
 exports.create = function(req,res){
-    Task.create({name: req.body.name, description: req.body.description, date: req.body.date, time: req.body.time})
-        .then(function(task) {
-            User.findOne({where: {id: req.user.id}}).then(function (user) {
-                user.addTasks(task).then(function() {
-                    res.json({                      //response with status 200
-                        success: true,
-                        message: 'Task added to database!',
-                        task: task
+    if (req.body.id){           //if the task already exists
+        Task.findOne({                      //get task by id
+            where: {id: req.body.id},
+            include: {model: User, attributes: [],where:{
+                id: req.user.id
+            }}
+        })
+            .then(function(task) {
+                task.updateAttributes({             //updating attributes
+                    name: req.body.name,
+                    description: req.body.description,
+                    date: req.body.date,
+                    time: req.body.time
+                });
+                res.json({                      //response with status 200
+                        success: true
                     });
+
+            })
+    }
+    else {          //creating the new task if it not exists
+        Task.create({name: req.body.name, description: req.body.description, date: req.body.date, time: req.body.time})
+            .then(function(task) {
+                User.findOne({where: {id: req.user.id}}).then(function (user) {
+                    user.addTasks(task).then(function() {
+                        res.json({                      //response with status 200
+                            success: true,
+                            message: 'Task added to database!',
+                            task: task
+                        });
+                    });
+                })
+            })
+            .catch(function(error){
+                res.json({
+                    success: false,
+                    message: error
                 });
             })
-        })
-        .catch(function(error){
-            res.json({
-                success: false,
-                message: error
-            });
-        })
+    }
+
 };
 
 //get the list of tasks
@@ -29,8 +52,10 @@ exports.getList = function(req,res){
         include: {model: User, attributes: [],where:{
             id: req.user.id
         }},
-        order: 'date ASC',
-        order: 'time ASC'
+        order: [
+            ['date', 'ASC'],
+            ['time', 'ASC']
+        ]
     })
         .then(function(tasks) {
             res.json({                      //response with status 200
@@ -49,9 +74,19 @@ exports.getTask = function(req,res){
         }}
     })
         .then(function(task) {
-            res.json({                      //response with status 200
-                success: true,
-                task: task
-            });
+            if (!task){
+                res.statusCode = 404;
+                res.json({
+                    success: false,
+                    message: "No task found!"
+                });
+            }
+            else{
+                res.json({                      //response with status 200
+                    success: true,
+                    task: task
+                });
+            }
+
         })
 };
